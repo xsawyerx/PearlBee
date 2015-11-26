e9e81cef36798b173277f3cb0536e65f9cd88ce4
Sawyer X <xsawyerx@cpan.org>
2015-11-26 17:44:53 +0100

    use config keyword

--- lib/PearlBee/Posts.pm
+++ lib/PearlBee/Posts.pm
@@ -6,11 +6,9 @@ use Dancer2::Plugin::DBIC;
 use PearlBee::Helpers::Util       qw<map_posts>;
 use PearlBee::Helpers::Pagination qw<get_total_pages get_previous_next_link>;
 
-my $config = config();
-
 prefix '/posts' => sub {
     get '' => sub {
-        my $nr_of_rows  = $config->{posts_on_page}   || 5; # Number of posts per page
+        my $nr_of_rows  = config->{posts_on_page}    || 5; # Number of posts per page
         my $page        = query_parameters->{'page'} || 1; # for paging
         my @posts       = resultset('Post')->search({ status => 'published' },{ order_by => { -desc => "created_date" }, rows => $nr_of_rows, page => $page });
         my $nr_of_posts = resultset('Post')->search({ status => 'published' })->count;
