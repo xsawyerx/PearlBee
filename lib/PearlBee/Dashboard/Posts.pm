371f9f959bc7cf4dc0af0d33769e64b615c68702
Sawyer X <xsawyerx@cpan.org>
2015-12-29 12:09:59 +0100

    add more posts stuff

--- lib/PearlBee/Dashboard/Posts.pm
+++ lib/PearlBee/Dashboard/Posts.pm
@@ -9,18 +9,30 @@ use PearlBee::Helpers::Pagination qw<
     generate_pagination_numbering
 >;
 
+use URI::Escape;
+
 prefix '/dashboard/posts' => sub {
     get '' => needs_permission view_post => sub {
         my $page        = query_parameters->{'page'} || 1;
+        my $status      = query_parameters->{'status'};
         my $nr_of_rows  = 5;
-        my @posts       = resultset('Post')->search( {}, { order_by => { -desc => 'created_date' }, rows => $nr_of_rows, page => $page } );
+        my $search_parameters = $status ? { status => $status } : {};
+
+        my @posts       = resultset('Post')->search( $search_parameters, { order_by => { -desc => 'created_date' }, rows => $nr_of_rows, page => $page } );
         my $count       = resultset('View::Count::StatusPost')->first;
 
         my ($all, $publish, $draft, $trash) = $count->get_all_status_counts;
 
+        # FIXME: temporary override of $all because "ugh"
+        #        Uses the View::Count::StatusPost
+        #        which doesn't allow specifying an optional post status
+        #        why have two methods instead of a method with a parameter?
+        $status and $all = $count->get_status_count($status);
+
+        my $action_url = "/dashboard/posts?status=" . uri_escape($status);
         # Calculate the next and previous page link
         my $total_pages                 = get_total_pages($all, $nr_of_rows);
-        my ($previous_link, $next_link) = get_previous_next_link($page, $total_pages, '/admin/posts');
+        my ($previous_link, $next_link) = get_previous_next_link($page, $total_pages, $action_url);
 
         # Generating the pagination navigation
         my $total_posts     = $all;
@@ -37,14 +49,17 @@ prefix '/dashboard/posts' => sub {
             all           => $all,
             page          => $page,
             next_link     => $next_link,
+            status        => $status,
             previous_link => $previous_link,
-            action_url    => 'admin/posts/page',
+            action_url    => $action_url,
             pages         => $pagination->pages_in_set
         } => { layout => 'admin' };
     };
 
     get '/new' => needs_permission create_post => sub {
-        1;
+        template 'admin/posts/add' => {
+            categories => [ resultset('Category')->all() ],
+        } => { layout => 'admin' };
     };
 
     post '/new' => needs_permission create_post => sub {
