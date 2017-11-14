12a50e08d7c4372aa48cf1e4231348665c26faac
Andre Walker <andre@andrewalker.net>
2017-11-14 10:55:28 +0100

    Update RBAC plugin to new Dancer2 plugin system

--- lib/Dancer2/Plugin/Auth/PearlBee.pm
+++ lib/Dancer2/Plugin/Auth/PearlBee.pm
@@ -5,36 +5,40 @@ use warnings;
 use Dancer2::Plugin;
 use URI::Escape;
 
-register needs_permission => sub {
-    my ( $dsl, $permission, $sub ) = plugin_args(@_);
-    my $rbac          = $dsl->config->{'rbac'};
+has 'rbac' => (
+    'is'          => 'ro',
+    'from_config' => 1,
+    'default'     => sub { +{} },
+);
+
+sub needs_permission :PluginKeyword {
+    my ( $self, $permission, $sub ) = @_;
+    my $rbac          = $self->rbac;
     my $error_message = uri_escape( $rbac->{'error_message'} )
         || "You do not have permission ($permission) to access the page";
 
     return sub {
-        my $user = $dsl->vars->{'user'}
-            or $dsl->app->redirect('/login?failure="Unauthorized user"');
+        my $user = $self->dsl->vars->{'user'}
+            or $self->dsl->redirect('/login?failure="Unauthorized user"');
 
         $rbac->can_role( $user->role, $permission )
-            or $dsl->app->redirect("/login?failure=\"$error_message\"");
-
-        goto &$sub;
-        }
-};
-
-register needs_role => sub {
-    my ( $dsl, $role, $sub ) = @_;
-    return sub {
-        my $user = $dsl->vars->{'user'}
-            or $dsl->app->redirect('/login');
-
-        $user->role eq $role
-            or $dsl->app->redirect('/login');
+            or $self->dsl->redirect("/login?failure=\"$error_message\"");
 
         goto &$sub;
     };
-};
+}
 
-register_plugin;
+sub needs_role :PluginKeyword {
+    my ( $self, $role, $sub ) = @_;
+    return sub {
+        my $user = $self->dsl->vars->{'user'}
+            or $self->dsl->redirect('/login');
+
+        $user->role eq $role
+            or $self->dsl->redirect('/login');
+
+        goto &$sub;
+    };
+}
 
 1;
